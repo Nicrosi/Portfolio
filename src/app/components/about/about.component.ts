a0d3f6efb6a1c4f66f5dd07519171eb4d67b9846
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2024-01-11 16:21:42 -0300

    about text

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -8,6 +8,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class AboutComponent {
   @Output() closeAbout = new EventEmitter<void>();
 
+  myText: string =
+    "Hello, I'm Guillermo Nicrosi, welcome to my portfolio!\nAs you can probably tell, it's an Angular based replica of Windows 11, but why?\n\nWell, after recently upgrading my system to W11, I became quite fond of it's sleek design and quality of life features, so I became inspired to base my portfolio on it!\n\nPlease take your time exploring, there are still features I've yet to add, but I hope you enjoy your time here nonetheless!";
+
   public onCloseAbout(): void {
     this.closeAbout.emit();
   }
